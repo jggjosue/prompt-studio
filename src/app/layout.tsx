b94235a0a31979865375df73bb63b1029dde9fbf
Josue Glez <federico01xdz@gmail.com>
2026-01-22 18:44:28 +0000

    agrega este tag a la pagina: <!-- Google tag (gtag.js) --> <script async

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { FirebaseClientProvider } from '@/firebase';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Prompt Studio',
@@ -28,6 +29,19 @@ export default function RootLayout({
         />
       </head>
       <body className="font-body antialiased" suppressHydrationWarning>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-8S22HHJK76"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-8S22HHJK76');
+          `}
+        </Script>
         <FirebaseClientProvider>
           {children}
           <Toaster />
