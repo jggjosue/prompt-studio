ff36505a2d472c5aba17b735465f050cebd59b13
Josue Glez <federico01xdz@gmail.com>
2026-02-02 17:20:29 +0000

    Copia y pega el código de AdSense entre las etiquetas <head> y </head> d

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,11 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Space+Grotesk:wght@400;500;700&display=swap"
           rel="stylesheet"
         />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7333662372662914"
+          crossOrigin="anonymous"
+        ></script>
       </head>
       <body className="font-body antialiased" suppressHydrationWarning>
         <Script
