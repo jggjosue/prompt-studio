8103d911520b77e44990cf8b0180c263797dc186
Josue Glez <federico01xdz@gmail.com>
2026-02-07 01:33:41 +0000

    conecta la cuenta de firebase: // Import the functions you need from the

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,7 +4,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyBjQULaRfWqFzVsmGVhmiEedyFo9X2Mrts",
   authDomain: "prompt-studio-dev.firebaseapp.com",
   projectId: "prompt-studio-dev",
-  storageBucket: "prompt-studio-dev.appspot.com",
+  storageBucket: "prompt-studio-dev.firebasestorage.app",
   messagingSenderId: "270633349599",
   appId: "1:270633349599:web:1850f1f1530af84170146a",
   measurementId: "G-8S22HHJK76"
