79f5620d78d7608924c09425a78277570e6f5ce5
Josue Glez <federico01xdz@gmail.com>
2026-02-07 02:44:52 +0000

    agrega en un script para guardar en la base de datos de firebase todos l

--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -4,22 +4,32 @@ import { NextResponse } from 'next/server';
 import { db } from '@/firebase/config';
 import { collection, doc, writeBatch } from 'firebase/firestore';
 import { PlaceHolderImages } from '@/lib/placeholder-images';
+import { PlaceHolderVideos } from '@/lib/placeholder-videos';
 
 export async function GET() {
   console.log('Seeding database...');
   try {
-    const placeholderImagesCollection = collection(db, 'placeholderImages');
     const batch = writeBatch(db);
 
+    // Seed Images
+    const placeholderImagesCollection = collection(db, 'placeholderImages');
     PlaceHolderImages.forEach((image) => {
-      // Use image.id as the document ID
       const docRef = doc(placeholderImagesCollection, image.id);
       batch.set(docRef, { title: image.title });
     });
 
+    // Seed Videos
+    const placeholderVideosCollection = collection(db, 'placeholderVideos');
+    PlaceHolderVideos.forEach((video) => {
+      const docRef = doc(placeholderVideosCollection, video.id);
+      batch.set(docRef, { title: video.title, likes: 0 });
+    });
+
     await batch.commit();
-    console.log('Database seeded successfully!');
-    return NextResponse.json({ message: 'Database seeded successfully!' });
+    console.log('Database seeded successfully with images and videos!');
+    return NextResponse.json({
+      message: 'Database seeded successfully with images and videos!',
+    });
   } catch (error: any) {
     console.error('Error seeding database:', error);
     return NextResponse.json(
